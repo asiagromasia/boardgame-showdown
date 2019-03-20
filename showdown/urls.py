629003e9d2274387d544a33c78fd11fce66670f2
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2019-03-19 22:07:19 -0700

    Adds details to games, events, participants

--- showdown/urls.py
+++ showdown/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('boardgames/', views.boardgames, name='boardgames'),
+    path('gamedetails/<int:id>', views.gamedetail, name='gamedetails'),
+    path('participants/', views.participant, name ='participant'),
+    path('participantdetails/<int:id>', views.participantdetail, name='participantdetail'),
+    path('event/', views.event, name ='event'),
+    path('eventdetails/<int:id>', views.eventdetail, name='eventdetail'),
 ]
